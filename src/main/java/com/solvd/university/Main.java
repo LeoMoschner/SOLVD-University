84ce6365dfe156820739b057459295a3d8cee449
Leandro Moschner <l.espindolamoschner@gmail.com>
2022-04-25 23:48:37 -0300

    Initial commit

--- src/main/java/com/solvd/university/Main.java
+++ src/main/java/com/solvd/university/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
 
-        /** git test*/
         while (MenuManager.menuManagement()) {
         }
     }
